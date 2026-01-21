2695158819d7da0d299fa6c6812379616ee9aca4
kuuhakumu <play.immin00@gmail.com>
2026-01-21 16:31:13 +0700

    no

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -5,5 +5,7 @@ const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
 export async function mapPhotoToAlbum(userIds?: number[]){
-    const response = await axios.get(PHOTOS_URL)
+    const responsePhoto = await axios.get(PHOTOS_URL)
+    const responseAlubums = await axios.get(ALBUMS_URL)
+    
 }
